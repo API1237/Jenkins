bbb436963b8ca20fca965e7e2b2fbb5d866aab96
rajat sharma <rajat.sharma@apiseroinc.onmicrosoft.com>
2021-06-15 15:41:33 +0530

    update

--- input.java
+++ input.java
@@ -5,12 +5,8 @@ public class input {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value of the first number ::");
 		int a = sc.nextInt();
-		System.out.println("Enter the value of the first number ::");
-		int b = sc.nextInt();
-		int result = a + b;
-		System.out.println("Sum of the given two numbers is ::" + result);
+		System.out.println("Sum of the given two numbers is ::" + a);
 	}
 
 }
