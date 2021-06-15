b577560bfc1d9aef353e7ed23258a0d93b2571e1
rajat sharma <rajat.sharma@apiseroinc.onmicrosoft.com>
2021-06-15 15:08:07 +0530

    add

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of the first number ::");
+		int a = sc.nextInt();
+		System.out.println("Enter the value of the first number ::");
+		int b = sc.nextInt();
+		int result = a + b;
+		System.out.println("Sum of the given two numbers is ::" + result);
+	}
+
+}
